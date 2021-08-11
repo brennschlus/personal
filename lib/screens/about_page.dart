7e1bf01c042c3e26fa669d39edc15ea8427067f2
brennschlus <brennschluss@protonmail.com>
2021-08-11 17:49:35 +0300

    Add assests, screens and refactoring

---
+++ lib/screens/about_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  static String id = 'about_page';
+  const AboutPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/wishlist_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class WishlistPage extends StatelessWidget {
+  static String id = 'wishlist_page';
+  const WishlistPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
