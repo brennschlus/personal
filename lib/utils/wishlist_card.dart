e0b290297d1ad91e7390b797d9e2bdd56ddca046
brennschlus <brensschluss@protonmail.com>
2021-08-12 14:06:37 +0300

    further development of wishlist card

--- lib/utils/wishlist_card.dart
+++ lib/utils/wishlist_card.dart
@@ -1,33 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:html' as html;
 
 class WishlistCard extends StatelessWidget {
-  String assetImage;
-  String name;
-  String price;
+  final String assetImage;
+  final String name;
+  final String price;
+  final String url;
 
-  WishlistCard(
-    this.assetImage,
-    this.name,
-    this.price,
-  );
+  WishlistCard(this.assetImage, this.name, this.price, this.url);
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 30,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(10),
-        ),
+        borderRadius: BorderRadius.circular(15),
       ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
+      child: Stack(
+        alignment: Alignment.center,
         children: [
           Image.asset(
             assetImage,
-            fit: BoxFit.scaleDown,
+            fit: BoxFit.cover,
           ),
-          Column(
-            children: [Text('Name: $name'), Text('Price: $price')],
+          Padding(
+            padding: EdgeInsets.only(bottom: 15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'Name: $name',
+                  style: TextStyle(fontSize: 32),
+                ),
+                Text(
+                  'Price: $price',
+                  style: TextStyle(fontSize: 24),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    html.window.open(url, name);
+                  },
+                  child: Text('Где купить?'),
+                )
+              ],
+            ),
           )
         ],
       ),
