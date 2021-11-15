a05457696b8c576bfe48a0b743c220b64b43252b
brennschlus <brennschluss@protonmail.com>
2021-11-15 19:51:47 +0300

    redisign home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -16,61 +16,54 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        title: Text('Pavel Rodionov'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.pushNamed(context, AboutPage.id);
-            },
-            icon: Icon(Icons.account_circle),
-          ),
-          IconButton(
-            onPressed: () {
-              Navigator.pushNamed(context, WishlistPage.id);
-            },
-            icon: Icon(Icons.present_to_all),
-          ),
-          InkWell(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text(
-                'Wishlist',
-                style: TextStyle(
-                    fontSize: 15,
-                    decoration: isHovering
-                        ? TextDecoration.underline
-                        : TextDecoration.none),
-              ),
-            ),
-            onHover: (hovering) {
-              setState(() => isHovering = hovering);
-            },
-            onTap: () {
-              Navigator.pushNamed(context, WishlistPage.id);
-            },
-          )
-        ],
-      ),
-      body: Stack(
+      body: Column(
         children: [
           SizedBox(
-            width: MediaQuery.of(context).size.width,
+            height: 20,
+          ),
+          SizedBox(
+            width: 260,
             child: Container(
               child: Image.asset(
-                'images/myphoto.jpg',
+                'images/photo.png',
               ),
             ),
           ),
           Align(
-            alignment: Alignment.centerLeft,
+            alignment: Alignment.center,
             child: Text(
-              'Pavel Rodionov donda donda donda',
+              'Pavel Rodionov',
               style: TextStyle(
-                fontSize: 36,
-                color: Colors.white,
+                fontSize: 46,
+                color: Colors.black,
               ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                InkWell(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Text(
+                      'Wishlist',
+                      style: TextStyle(
+                          fontSize: 15,
+                          decoration: isHovering
+                              ? TextDecoration.underline
+                              : TextDecoration.none),
+                    ),
+                  ),
+                  onHover: (hovering) {
+                    setState(() => isHovering = hovering);
+                  },
+                  onTap: () {
+                    Navigator.pushNamed(context, WishlistPage.id);
+                  },
+                )
+              ],
             ),
           )
         ],
